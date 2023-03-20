5dadceba55e24bb53ae1a3bc43e8b2046d73182f
Erwin Marte <erwin.mdq@gmail.com>
2023-03-20 13:17:03 -0300

    Add remove stars function with blackHole

--- functions/stars_functions.js
+++ functions/stars_functions.js
@@ -40,117 +40,110 @@ function create_star(){
         let colour = Math.floor(Math.random() * colours.length);
         let scale = Math.floor(Math.random() * scales.length);
 
-        // Create star
-        let star_create = document.createElement("div");   
-        star_create.setAttribute("class", "stars");
-        star_create.setAttribute("id", "star");
-        doc.appendChild(star_create);
+        // Check number of stars in the DOM actually
+        let starDivs = document.querySelectorAll('div[id="star"]');
+        let numStarDivs = starDivs.length;
 
-        // Styles of the star
-        star_create.style.left = `${y}px`;
-        star_create.style.top = `${x}px`;
-        star_create.style.height = `${scales[scale]}px`;
-        star_create.style.width = `${scales[scale]}px`;
-        star_create.style.background = colours[colour];
+        // If the stars are less than 50 it create the star
+        if (numStarDivs < 300){
+            // Create star
+            let star_create = document.createElement("div");   
+            star_create.setAttribute("class", "stars");
+            star_create.setAttribute("id", "star");
+            doc.appendChild(star_create);
 
-        // Depends the scale of the star the scale of the shadow blur and spread
-        if(scales[scale] < 1){
-            var blur_radius = 3;
-            var spread_radius = 0.7;
-        }else if(scales[scale] <= 2){
-            var blur_radius = 2;
-            var spread_radius = 1;
-        }else{
-            var blur_radius = 5;
-            var spread_radius = 2;
+            // Styles of the star
+            star_create.style.left = `${y}px`;
+            star_create.style.top = `${x}px`;
+            star_create.style.height = `${scales[scale]}px`;
+            star_create.style.width = `${scales[scale]}px`;
+            star_create.style.background = colours[colour];
+
+            // Depends the scale of the star the scale of the shadow blur and spread
+            if(scales[scale] < 1){
+                var blur_radius = 3;
+                var spread_radius = 0.7;
+            }else if(scales[scale] <= 2){
+                var blur_radius = 2;
+                var spread_radius = 1;
+            }else{
+                var blur_radius = 5;
+                var spread_radius = 2;
+            }
+            star_create.style.boxShadow = `0px 0px ${blur_radius}px ${spread_radius}px ${colours[colour]}`;
+
+            // Calculate the area of the stars(DIVs) and the area of the universe(DOM)
+            let area = area_calc(scales[scale]);
+            area_of_the_stars.push(area);
+
+            dom_area = dom_info.width * dom_info.height;
         }
-        star_create.style.boxShadow = `0px 0px ${blur_radius}px ${spread_radius}px ${colours[colour]}`;
-
-        // Calculate the area of the stars(DIVs) and the area of the universe(DOM)
-        let area = area_calc(scales[scale]);
-        area_of_the_stars.push(area);
-        
-        dom_area = dom_info.width * dom_info.height;
-    }
+    };
     
     // If the "stars" area in total equals or is bigger than 10% of the total DOM area, create a black hole, to "DELETE" some of the stars
-    let total_area_stars = sum_total(area_of_the_stars);
-    let dom_specific_percentage = percent_of_x(dom_area, 0.05);
+    //let total_area_stars = sum_total(area_of_the_stars);
+    //let dom_specific_percentage = percent_of_x(dom_area, 0.01);
 
     // Create the black hole
-    if(total_area_stars < dom_specific_percentage){
+    if(document.querySelectorAll("div").length <= 150){
         let black_hole = document.createElement("div");
         black_hole.setAttribute("class", "black_hole");
         black_hole.setAttribute("id", "hole");
         doc.appendChild(black_hole);
         black_hole.style.left = `${y}px`;
         black_hole.style.top = `${x}px`;
-    }
+    };
 
-    stars = document.querySelectorAll("div");   
+    stars = document.querySelectorAll('div');  
 };
 
 // Constant movement for stars/black hole
 function moves() {
-    setInterval(() => {
-        stars.forEach(star =>{
+    function animate() {
+        stars.forEach(star => {
             let actual_left = parseInt(star.style.left);
             let actual_top = parseInt(star.style.top);
-            let new_top = actual_top + Math.floor(Math.random() * Math.PI   *1);
-            let new_left = actual_left + Math.floor(Math.random() * Math.PI *1);
-            if(new_top % 3 == 0){
-                star.style.top = new_top +   "px";
+            let new_top = actual_top + Math.floor(Math.random() * Math.PI * 1);
+            let new_left = actual_left + Math.floor(Math.random() * Math.PI * 1);
+            if (new_top % 3 == 0) {
+                star.style.top = new_top + "px";
                 star.style.left = new_left + "px";
-            }else{
-                star.style.top = new_top - Math.PI*0.5 +"px";
-                star.style.left = new_left - Math.PI*0.5 +"px";
+            } else {
+                star.style.top = new_top - Math.PI * 0.50 + "px";
+                star.style.left = new_left - Math.PI * 0.50 + "px";
             };
         });
-    }, 300);
-}
+        requestAnimationFrame(animate);
+    };
+    requestAnimationFrame(animate);
+};
 
 // Black hole consum funtions
 function black_hole_consume() {
-    let hole = document.getElementById("hole");
-    let y_position = parseInt(hole.style.left); //width
-    let x_position =  parseInt(hole.style.top); //height
+    let hole = document.getElementById("hole");    
+    let blackHoleRect = hole.getBoundingClientRect();
+    let starDivs = document.querySelectorAll('div[id="star"]');
 
-
-    // a completar, si el eje x o y consinsiden con la posicion del egujero negro, el div de la estrella debe ser removido
-    stars.forEach(star =>{
-        if(star.className == "stars"){
-            let star_y_position = parseInt(star.style.left);
-            let star_x_position = parseInt(star.style.top);
-            if(star_y_position === y_position && star_x_position === x_position){
-                console.log(star_y_position,star_x_position)
-                console.log(y_position,x_position)
-                //console.log(star)
-                star.style.visibility = "red";
-                console.log(star)
-            }
-        }
-    })
-}
-
-// Exe the functions
-const check = () =>{
-    stars.forEach(star =>{//no esta funcionando correctamente
-        console.log(star.className)
-        if(star.className == "stars"){
-            switch (count) {
-                case count < 300:
-                    console.log(count);
-                    doc.onclick = create_star;
-                    break;
-                case count >= 300:
-                    doc.onclick = console.log("count > 300");
-                    break;
-            }
+    starDivs.forEach(star =>{
+        let divStar = star.getBoundingClientRect();
+        if (blackHoleRect.x === divStar.x && blackHoleRect.y === divStar.y) {
+            console.log(star.id + " removed");
+            console.log(starDivs.length)
+            star.remove();
         };
     });
 };
-doc.onclick = check;
+function animate_blackHola (){
+    black_hole_consume();
+    requestAnimationFrame(animate_blackHola)
+}
+
+
+
 setTimeout(() => {
     create_star();
     moves();
+    animate_blackHola();
 }, 1500);
+
+doc.onclick = create_star;
