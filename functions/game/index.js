17a410ffe57d3389ae8969730b8eddfe64477bc8
Erwin Marte <erwin.mdq@gmail.com>
2023-05-03 14:43:54 -0300

    update game UI

--- functions/game/index.js
+++ functions/game/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
-let actualPoints = document.getElementById("actualPoints")
+let actualPoints = document.getElementById("actualPoints");
+let actualLives = document.getElementById("actualLives");
+let lives = 3;
 let angle = 0;
 let mouseX;
 let mouseY;
@@ -72,7 +74,7 @@ function createSmallSquare() {
 // space cat
 let space_cat = new Image();
 let meteor = new Image();
-space_cat.src = "./style/assets/cat-driver_1.png"; // ver imagen por una hd
+space_cat.src = "./style/assets/space_cat_DALLE.png"; // ver imagen por una hd
 meteor.src = "./style/assets/meteor.png";
 // Dibujar
 function draw() {
@@ -83,7 +85,7 @@ function draw() {
   tipY = squareY + 25 + 20 * Math.sin(angle);
   ctx.translate(tipX, tipY);
   ctx.rotate(angle + Math.PI / 2);
-  ctx.drawImage(space_cat, -95, -5, 100 , 125); // manejar el tamaño de la imagen aca > y de donde dispara
+  ctx.drawImage(space_cat, -95, -40, 120 , 135); // manejar el tamaño de la imagen aca > y de donde dispara
   ctx.rotate(-angle - Math.PI / 2);
   ctx.translate(-tipX, -tipY);
 
@@ -104,20 +106,36 @@ function draw() {
   }
 }
 
-  // red circle > shot
+  // red circle > shot > OLD SHOT, circle shot not LASER
+  //if (shouldDrawCircle) {
+  //  // Dibuja el círculo rojo
+  //  let pointinX = circleX += Math.cos(direction) * 10;
+  //  let pointinY = circleY += Math.sin(direction) * 10;
+  //  for(let i=0; i<circleList.length; i++){
+  //    ctx.beginPath();
+  //    ctx.fillStyle = 'red';
+  //    ctx.arc(pointinX, pointinY, 3, 0, 2 * Math.PI);
+  //    ctx.fill();
+  //  }
+//
+  //  checkCollision();
+  //}
   if (shouldDrawCircle) {
-    // Dibuja el círculo rojo
-    let pointinX = circleX += Math.cos(direction) * 10;
-    let pointinY = circleY += Math.sin(direction) * 10;
-    for(let i=0; i<circleList.length; i++){
-      ctx.beginPath();
-      ctx.fillStyle = 'red';
-      ctx.arc(pointinX, pointinY, 3, 0, 2 * Math.PI);
-      ctx.fill();
-    }
-
+    // Dibuja la línea roja
+    let lineLength = 50; // longitud de la línea
+    let lineWidth = 5; // grosor de la línea
+    let pointinX = circleX + Math.cos(direction) * lineLength;
+    let pointinY = circleY + Math.sin(direction) * lineLength;
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = lineWidth;
+    ctx.moveTo(circleX, circleY);
+    ctx.lineTo(pointinX, pointinY);
+    ctx.stroke();
+  
     checkCollision();
   }
+  
 }
 
 // Función para revisar colisión entre el círculo y los cuadrados pequeños
@@ -133,7 +151,6 @@ function checkCollision() {
       smallSquares.splice(i, 1);
       count = count + 1;
       console.log(count)
-      actualPoints.innerHTML = `<th>Points : ${count}</th>`
     }
   }
 
@@ -156,6 +173,21 @@ function checkCollision() {
 // Función para destruir el triángulo principal
 function destroyTriangle() {
 
+  // Contavilizador de vidas
+  lives = lives - 1;
+  console.log(lives)
+  actualPoints.innerHTML = `<th>Points : ${count}</th>`;
+  if(lives === 3){
+    actualLives.innerHTML = `<th>${lives.lives} ${lives.lives} ${lives.lives}</th>`;
+  }else if(lives===2){
+    actualLives.innerHTML = `<th>${lives.lives} ${lives.lives}</th>`;
+  }else if (lives===1){
+    actualLives.innerHTML = `<th>${lives.lives}</th>`;
+  }else if (lives === 0){
+    actualLives.innerHTML = `<th> </th>`;
+  }
+
+  // Destruccion del gato espacial > ex triangulo
   ctx.beginPath();
   ctx.arc(tipX, tipY, 30, 0, 2 * Math.PI);
   ctx.fillStyle = "black";
@@ -168,20 +200,73 @@ function destroyTriangle() {
 
 // Detectar cuando se presionan las teclas de flecha
 // Detectar cuando se presionan las teclas de flecha o WASD
+let keysPressed = {}; // variable para guardar las teclas presionadas
+
 document.addEventListener("keydown", function(event) {
-  if (event.code === "ArrowUp" || event.code === "KeyW") {
-    squareY -= 3;
-  } else if (event.code === "ArrowRight" || event.code === "KeyD") {
-    squareX += 3;
-  } else if (event.code === "ArrowDown" || event.code === "KeyS") {
-    squareY += 3;
-  } else if (event.code === "ArrowLeft" || event.code === "KeyA") {
-    squareX -= 3;
+  keysPressed[event.code] = true; // agregar la tecla presionada a la variable
+});
+
+document.addEventListener("keyup", function(event) {
+  delete keysPressed[event.code]; // eliminar la tecla presionada de la variable
+});
+
+setInterval(function() {
+  const speed = 0.75; // velocidad
+  let dx = 0;
+  let dy = 0;
+
+  // verificar las teclas presionadas y calcular el movimiento en consecuencia
+  if (keysPressed["ArrowUp"] || keysPressed["KeyW"]) {
+    dy -= speed;
+  }
+  if (keysPressed["ArrowRight"] || keysPressed["KeyD"]) {
+    dx += speed;
+  }
+  if (keysPressed["ArrowDown"] || keysPressed["KeyS"]) {
+    dy += speed;
+  }
+  if (keysPressed["ArrowLeft"] || keysPressed["KeyA"]) {
+    dx -= speed;
   }
 
-  //console.log("Posicion del mouse: " + mouseX +" "+ mouseY);
-  //console.log("Posicion del triangulo: " + squareY + " " + squareX);
-});
+  // si se están presionando dos teclas de dirección, ajustar el movimiento en consecuencia
+  if ((keysPressed["ArrowUp"] || keysPressed["KeyW"]) && (keysPressed["ArrowRight"] || keysPressed["KeyD"])) {
+    dx *= Math.sqrt(0.5);
+    dy *= Math.sqrt(0.5);
+  }
+  if ((keysPressed["ArrowUp"] || keysPressed["KeyW"]) && (keysPressed["ArrowLeft"] || keysPressed["KeyA"])) {
+    dx *= Math.sqrt(0.5);
+    dy *= Math.sqrt(0.5);
+  }
+  if ((keysPressed["ArrowDown"] || keysPressed["KeyS"]) && (keysPressed["ArrowRight"] || keysPressed["KeyD"])) {
+    dx *= Math.sqrt(0.5);
+    dy *= Math.sqrt(0.5);
+  }
+  if ((keysPressed["ArrowDown"] || keysPressed["KeyS"]) && (keysPressed["ArrowLeft"] || keysPressed["KeyA"])) {
+    dx *= Math.sqrt(0.5);
+    dy *= Math.sqrt(0.5);
+  }
+
+  // mover el cuadrado según el movimiento calculado
+  squareX += dx;
+  squareY += dy;
+}, 10); // ejecutar la verificación de teclas cada 10 milisegundos
+
+// OLD MOVIMENT not diagonal moviment here
+//document.addEventListener("keydown", function(event) {
+//  if (event.code === "ArrowUp" || event.code === "KeyW") {
+//    squareY -= 3;
+//  } else if (event.code === "ArrowRight" || event.code === "KeyD") {
+//    squareX += 3;
+//  } else if (event.code === "ArrowDown" || event.code === "KeyS") {
+//    squareY += 3;
+//  } else if (event.code === "ArrowLeft" || event.code === "KeyA") {
+//    squareX -= 3;
+//  }
+//
+//  //console.log("Posicion del mouse: " + mouseX +" "+ mouseY);
+//  //console.log("Posicion del triangulo: " + squareY + " " + squareX);
+//});
 
 //mouse
 canvas.addEventListener('mousemove', function(event) {
@@ -192,7 +277,7 @@ canvas.addEventListener('mousemove', function(event) {
   mouseY = event.clientY - rect.top;
 
   // Calcular el ángulo de rotación del triángulo en base a la posición del mouse
-  angle = Math.atan2(mouseY - (squareY + 240), mouseX - (squareX + 240)); // > a donde apunta mira del mouse
+  angle = Math.atan2(mouseY - (squareY + 1000), mouseX - (squareX + 240)); // > a donde apunta mira del mouse se mueve la imagen 
 });
 
 // disparo
@@ -231,7 +316,7 @@ canvas.addEventListener("click", function(event){
 // Agregar cuadrados pequeños aleatorios cada 2 segundos
 setInterval(function() {
   createSmallSquare();
-}, 80); //Crear mas o menos cuadrados
+}, 800); //Crear mas o menos cuadrados
 
 // Función para animar el canvas
 function animate() {
