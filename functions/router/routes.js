1aa0b6de447f74cc05a12108258b97768ef7ae86
Erwin Marte <erwin.mdq@gmail.com>
2023-06-07 21:01:55 -0300

    Router > spa

--- functions/router/routes.js
+++ functions/router/routes.js
@@ -1,9 +1,25 @@
+"use strict";
+
 function loadPage(page) {
   fetch(`./pages/${page}.html`)
     .then(response => response.text())
     .then(html => {
       const app = document.getElementById('app');
       app.innerHTML = html;
+      // Load Files
+      const scriptTags = app.getElementsByTagName('script');
+      for (let i = 0; i < scriptTags.length; i++) {
+        const scriptTag = document.createElement('script');
+        scriptTag.src = scriptTags[i].src;
+        document.body.appendChild(scriptTag);
+      }
+      const linkTags = app.getElementsByTagName('link');
+      for (let i = 0; i < linkTags.length; i++) {
+        const linkTag = document.createElement('link');
+        linkTag.href = linkTags[i].href;
+        linkTag.rel = 'stylesheet';
+        document.head.appendChild(linkTag);
+      }
     })
     .catch(error => console.error(error));
 }
@@ -12,13 +28,13 @@ function handleRoute() {
   const path = window.location.pathname;
 
   switch (path) {
-    case './':
+    case '/':
       loadPage('index');
       break;
-    case './contact':
+    case '/contact':
       loadPage('contact');
       break;
-    case './game':
+    case '/game':
       loadPage('game');
       break;
     default:
